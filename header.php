298402c081f58f3791cbbd6c15b832aa36eff019
slizano <sofia@lizano.org>
2015-08-13 16:15:17 -0400

    started building strategix

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+<link href="http://fast.fonts.net/cssapi/c0791320-eb3a-41c7-8868-c0ceb0eb6a90.css" type="text/css" rel="stylesheet"/>
+<meta charset="UTF-8"> 
+<title>Strategix Consulting</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+<link href="css/reset.css" rel="stylesheet" type="text/css">
+<link href="css/main.css" rel="stylesheet" type="text/css">
+<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Istok+Web' rel='stylesheet' type='text/css'>
+ 
+</head>
+
+<body>
+
+<div class="slide-section">
+	<div class=" container">
+			<h1>Helping you make better real estate decisions</h1>
+			<h2>Sed ut perspiciatis unde omnis iste natus error sit voluptatem</h2>
+	</div>
+</div>
+
+<header>
+	<div class="menu">
+		<div id="nav">
+			<div class="logo"><a href="index.php"><img src="images/logo-2x.png"></a>
+			</div>
+			<ul class="navigation">
+				<li id="services_btn"><a href="#">About</a></li>
+                <li id="projects_btn"><a href="#">Case Studies</a></li>
+                <li id="about_btn"><a href="#">Profile</a></li>
+                <li id="contact_btn"><a href="#">Contact</a></li>
+			</ul>
+		</div>
+	</div>
+</header>
---
+++ index.php
@@ -0,0 +1,78 @@
+<?php include 'header.php'; ?>
+<div class="about">
+	<div class="container-about">
+		<section class="about">
+			<h2>About</h2>
+
+		    <article class="about-info">
+		       	<p class="left">STRATEGIX is a professional services firm that provides Development, Owners Representation and Advisory services of strategic and tactical value to clients undertaking design, construction and real estate development projects throughout North America.
+<br><br>We partner with our clients in the development and implementation of bespoke project methodologies and best in class business solutions to optimize resource utilization, project execution and delivery across real estate portfolios of all sizes.
+<br><br><p class="right">STRATEGIX integrates the expertise of a broad variety of project stakeholders including technical professionals, vendors, consultants and recognized industry leaders, alongside our client's senior executive and in-house facilities teams.  Using state of the art technology and methods, we build long lasting, versatile, smart and appropriate solutions for occupancies of all kinds.</p>
+		    </article>
+		</section>
+		<div class="services">
+		    <section class="services">
+		        <article>
+		            <h2>Services</h2>
+			            <dl>
+			                <dt>Market Sectors</dt>
+			                <dd>Corporate</dd>
+			                <dd>Government</dd>
+			                <dd>Healthcare</dd>
+			                <dd>Higher Education</dd>
+			                <dd>Hospitality</dd>
+			                <dd>Residential</dd>
+			                <dd>Science + Technology</dd>
+			                <dd>Transportation</dd>
+			            </dl>
+		            <dl>
+		                <dt>Development</dt>
+		                <dd>Site Selection</dd>
+		                <dd>Master Planning</dd>
+		                <dd>Zoning</dd>
+		                <dd>Feasibility Analysis</dd>
+		            </dl>
+		            <dl>
+		                <dt>Owners Representative</dt>
+		                <dd>Budget Development</dd>
+		                <dd>Constructability Reviews</dd>
+		                <dd>Value Engineering</dd>
+		                <dd>Cost Estimating</dd>
+		                <dd>Scheduling</dd>
+		                <dd>Sustainability</dd>
+		                <dd>Bid Management</dd>
+		                <dd>Procurement</dd>
+		                <dd>Contract Development</dd>
+		                <dd>Quality Control</dd>
+		                <dd>Cost Control</dd>
+		                <dd>Close Out</dd>
+		            </dl>
+		            <dl>
+		                <dt>Advisory</dt>
+		                <dd>Operational Excellence</dd>
+		                <dd>Excellence</dd>
+		                <dd>Processes</dd>
+		                <dd>Organization</dd>
+		            </dl>
+		        </article>
+		    </section>
+		</div>
+	</div>
+</div>
+
+<div class="who">
+	<div class="container-who">
+	</div>
+</div>
+
+<div class="projects">
+	<div class="container-projects">
+	</div>
+</div>
+
+<div class="contact">
+	<div class="container-contact">
+	</div>
+</div>
+
+
